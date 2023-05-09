e13c8d98999afef2957fd00501671c523d292a39
on22-neleries <nel.ries.22@lehre.mosbach.dhbw.de>
2023-05-09 11:01:48 +0200

    API Einbindung + Sucheingabe + Suchliste

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+import { listenerCount } from "process";
+import { createVoidZero } from "../node_modules/typescript/lib/typescript";
+
+const storageTimeOutInMinutes: number = 0.5;
+
+const geocodingUrl: string = "https://geocoding-api.open-meteo.com/v1/search";
+const countOfCitySuggestions: number = 10;
+const languageFormat: string = "de";
+const searchInput = document.querySelector("#searchInput") as HTMLInputElement;
+
+const listElement = document.querySelector(
+  ".searchListItem"
+) as HTMLButtonElement;
+const listParent = document.querySelector(".searchListGroup") as HTMLElement;
