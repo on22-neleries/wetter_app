8c96556a4bec8802cb335e817381bb2b4186f4d1
on22-neleries <nel.ries.22@lehre.mosbach.dhbw.de>
2023-05-09 21:35:01 +0200

    Storage Key

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
-import { listenerCount } from "process";
-import { createVoidZero } from "../node_modules/typescript/lib/typescript";
+const storageTimeOutInMinutes: number = 10;
 
-const storageTimeOutInMinutes: number = 0.5;
 
 const geocodingUrl: string = "https://geocoding-api.open-meteo.com/v1/search";
 const countOfCitySuggestions: number = 10;
 const languageFormat: string = "de";
+
+const lastSearchedCityStorageKey ='lastSearchedCity';
 const searchInput = document.querySelector("#searchInput") as HTMLInputElement;
 
 const listElement = document.querySelector(
